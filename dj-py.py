d0eb11bcb4c0fe088dc8cc541a213865c7c7e31a
GiacomoPinardi <pinobo1997@gmail.com>
2015-04-30 00:20:49 +0200

    v 0.2

--- dj-py.py
+++ dj-py.py
@@ -2,7 +2,7 @@ from pygame import mixer
 import pygame
 import os
 from os import listdir
-from os.path import isdir, isfile
+from os.path import isdir, isfile, exists
 from string import lower
 
 global songs
@@ -29,33 +29,42 @@ def printHello () :
     print "***** github.com/GiacomoPinardi/dj-py *****\n"
 	
 def printHelp () :
-	print "Help Here!"
+    print "github.com/GiacomoPinardi/dj-py"
+    print "\n[init <path>]\n\tInit the folder with sound file\n\t<path> is the path's folder"
+    print "\n[h], [help]\n\tPrint the help page"
+    print "\n[quit]\n\tExit, return to shell"
+    print "\n[pause], [stop]\n\tPause the current song"
+    print "\n[play], [start]\n\tPlay the song previously paused"
+    print "\n[pl]\n\tChange song/page number"
+    print "\n[<page>:<text>]\n\tDisplay <page>th page of song with <text> in filename\n\t<page> number of sound found\n\t\tIf empty the first page is displayed\n\t<text> search song by text\n\t\tIf empty all files that contains '.' are displayed"
 
 def initFolder (p):
-	global path
-	path = p
-	try :
-		if (path[len(path)-1] != "/"):
-			path = path + "/"
+    global path
+    path = p
+    try :
+        if (path[len(path)-1] != "/"):
+            path = path + "/"
 
-		if (os.path.isdir(path)):
-			all_things = listdir(path)
-			del songs[:]
-			for element in all_things:
-				if (os.path.isfile(path + element)):
-					songs.append(element)
-			if (len(songs) != 0):
-				print "Successfull! [" + str(len(songs)) + "]"
-		else:
-			print path + " not a directory!"
-			c = raw_input("Continue [Y/N]: ")
-			if (c == "y" or c == "yes" or c == "Y"):
-				initFolder()
-	except OSError:
-		print "Error: Permisson Denied!\n"
+        if (os.path.isdir(path)):
+            all_things = listdir(path)
+            del songs[:]
+            for element in all_things:
+                if (os.path.isfile(path + element)):
+                    songs.append(element)
+            if (len(songs) != 0):
+                print "Successfull! [" + str(len(songs)) + "]"
+        else:
+            if (not os.path.exists(path)):
+                print path + " doesn't exist!"
+            else:
+                print path + " is not a directory!"
+            
+    except OSError:
+        print "Error: Permisson Denied!"
 
 def view (page):
     global PAGE_LENGTH
+    global songs
     pg = PAGE_LENGTH
     start = (pg * (page - 1))
     if (len(result) >= (pg * page)):
@@ -65,6 +74,9 @@ def view (page):
         end = (len(result) % pg) + (pg * (page - 1))
     else:
         print "Error: Not Enough Song!"
+        if (len(songs) == 0):
+            print "Should first [init<path>]"
+
         end = 0
 
     for x in range(start, end):
@@ -129,12 +141,17 @@ def loop () :
                 print "Error: Page Length Must Be a Number!"
         elif (c.find(":") != -1):            
             try:
-                
-                search(c)
+                if (c.find(":") == (len(c)-1)):
+                    if (len(c) == 1):
+                        listContains(".", 1)
+                    else:
+                        listContains(".", int(c[:c.find(":")]))
+                else:        
+                    search(c)
 
                 exit = False
                 while (not exit):
-                    number = raw_input("Number: ")
+                    number = raw_input("Song number: ")
                     if (number == "q"):
                         exit = True
                     elif (number.find(":") != -1) :
@@ -145,7 +162,11 @@ def loop () :
                         print "Error: Max number: " + str(len(result) - 1)
 
             except ValueError:
-                print "Error: Please only number as page/index!"    
+                print "Error: Please only number as page/index!"
+            except IndexError:
+                print "Error: Please only number as page/index!"
+        else:
+            print "Type [h] for help page." 
 
 # Main:
 printHello()
